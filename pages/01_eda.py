eeb2ba8aaf83d2b7a0432155827359b30cbaefdb
gunarka <gunar.kachel@gmail.com>
2025-11-20 17:12:17 +0100

    streamlit EDA page v0.1

--- pages/01_eda.py
+++ pages/01_eda.py
@@ -18,12 +18,13 @@ st.set_page_config(
 f.navigation()
 
 with duckdb.connect("data/team_data.duckdb") as conn:
-    df = conn.execute("SELECT * FROM df_train").fetchdf()
+    st.session_state.df_train = conn.execute("SELECT * FROM df_train").fetchdf()
     
 #df.head()  
 
-st.session_state.df = df
-
+#st.session_state.df_train = df_train
+df_train = st.session_state.df_train
+df_train["machine_failure"] = df_train["machine_failure"].astype("category")
 
 lab_dict = {
     "udi":"UDI",
@@ -57,6 +58,136 @@ pio.templates["custom_theme"] = go.layout.Template(
 
 pio.templates.default = 'custom_theme'
 
-st.title("📊 EDA Visualization")
+################## CONTENT START
+
+############ TITLE
+
+st.title("📊 Exploratory Data Analysis")
+
+
+
+###### SUBHEADER
+
 st.write("")
-st.subheader("Height vs Weight")
+st.subheader("Training Dataset")
+st.markdown("""
+    - **split:** training dataset represents  70% of all observations
+    - **shape:** 14 features x 7.000 observations
+    - **categorical values:** xyz
+    - **numerical values:** abc
+    
+    """)
+df_train
+###### SUBHEADER
+
+st.write("")
+st.subheader("Histogram of Numerical Data")
+st.markdown("Text...")
+
+#col_sort = "udi"
+col_grouping = ["machine_failure"]
+plot_list = [
+    "air_temperature_k", 
+    "process_temperature_k", 
+    "rotational_speed_rpm", 
+    "torque_nm", 
+    "tool_wear_min",
+    "machine_failure"
+]
+size = 500
+df_chart = df_train#.sort_values(col_sort)
+
+for column in plot_list:
+    for group in col_grouping:
+        fig = px.histogram(
+            data_frame = df_chart,
+            x = column,
+            color = group,
+            width = size,
+            height = size,
+            opacity=0.7,
+            barmode="overlay",
+            labels=lab_dict,
+            title=f"Histogram of {lab_dict[column]}"
+            )
+        fig.update_layout(legend=dict(
+        orientation="h",
+        yanchor="top",
+        y=-0.20,
+        ))
+        #fig.show()
+        st.plotly_chart(fig)
+
+
+###### SUBHEADER
+
+st.write("")
+st.subheader("Box Plot of Selected Data")
+st.markdown("Observation outside inter quartile range (IQR) will not be removed as the are part of the valid data distribution.")
+
+size = 400
+
+cols = [ 'air_temperature_k',
+ 'process_temperature_k',
+ 'rotational_speed_rpm',
+ 'torque_nm',
+ 'tool_wear_min',]
+df_chart = df_train
+for col in cols:
+    fig = px.box(
+        df_chart, 
+        y=col, 
+        width=size, 
+        height=size, 
+        labels=lab_dict,
+        )
+    #fig.show()
+    st.plotly_chart(fig)
+
+
+###### SUBHEADER
+
+st.write("")
+st.subheader("Scatter Plot of Selected Data")
+
+
+# individual scatter plots
+size = 400
+scatter_color ="machine_failure"
+df_chart = df_train#[df_train["machine_failure"]==1]
+
+scatter_pairs = {
+    1:[ 'rotational_speed_rpm','torque_nm',  scatter_color],
+    #2:[ 'torque_nm', 'tool_wear_min', scatter_color],
+    3:[ 'torque_nm', 'tool_wear_min',  scatter_color],
+    #4:['cons_number', 'udi',  scatter_color],
+    #5:[ 'udi','torque_nm',  scatter_color],
+    #6:[ 'cons_number','torque_nm',  scatter_color]
+    7:["air_temperature_k", "process_temperature_k",scatter_color ]
+    }
+
+
+for key, values in scatter_pairs.items():
+    fig = px.scatter(
+        df_chart,
+        x = values[0],
+        y = values[1],
+        color = values[2],
+        opacity = 0.7,
+        width=size,
+        height=size,
+        labels=lab_dict,
+        title=f"{lab_dict[values[0]]} vs {lab_dict[values[1]]}"
+
+        #        color_continuous_scale = ["green", "red"]
+    )
+    fig.update_layout(legend=dict(
+        orientation="h",
+        yanchor="top",
+        y=-0.30,
+   ))
+
+    #fig.show()
+    st.plotly_chart(fig)
+
+

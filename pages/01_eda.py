cf8da02edfe82fc58d10f54ccd084fbdd909f03a
gunarka <gunar.kachel@gmail.com>
2025-11-27 17:12:25 +0100

    eda: latest changes // streamlit: addition of slides example

--- pages/01_eda.py
+++ pages/01_eda.py
@@ -7,6 +7,10 @@ import duckdb
 from sklearn.model_selection import train_test_split
 import streamlit as st
 import st_function as f
+import math
+from pandas import set_option
+import reveal_slides as rs
+
 
 #repeat for every page to alter the default
 st.set_page_config(
@@ -17,15 +21,31 @@ st.set_page_config(
 
 f.navigation()
 
+# get data
 with duckdb.connect("data/team_data.duckdb") as conn:
     st.session_state.df_train = conn.execute("SELECT * FROM df_train").fetchdf()
     
-#df.head()  
-
 #st.session_state.df_train = df_train
 df_train = st.session_state.df_train
-df_train["machine_failure"] = df_train["machine_failure"].astype("category")
 
+# generate additional columns
+# power measured in W = 1Nm/s // torque in Nm // rotational speed in rpm => * 60 (sec) / 2pi (one ration)
+df_train["power_w"] = (df_train["torque_nm"] / (df_train["rotational_speed_rpm"] * 60 / (2 * math.pi))) * 1000
+df_train["delta_temp_k"] = df_train["process_temperature_k"]-df_train["air_temperature_k"]
+
+# set categorical columns
+cat_cols =[
+    "machine_failure",
+    "twf",
+    "hdf",
+    "pwf",
+    "osf",
+    "rnf"]
+
+for col in cat_cols:
+    df_train[col] = df_train[col].astype("category")
+
+# define dictionary for labels
 lab_dict = {
     "udi":"UDI",
     "product_id":"Product ID",
@@ -36,21 +56,26 @@ lab_dict = {
     'torque_nm':'Torque [Nm]',
     'tool_wear_min':'Tool wear [min]',
     "machine_failure":"Machine failure",
-    "twf":"TWF",
-    "hdf":"HDF",
-    "pwf":"PWF",
-    "osf":"OSF",
-    "rnf":"RNF"
+    "twf":"Tool Wear Failure",
+    "hdf":"Heat Dissipation  Failure",
+    "pwf":"Power Failure",
+    "osf":"Over Strain Failure",
+    "rnf":"Random Failure",
+    "delta_temp_k": "Temperature Difference [K]",
+    "power_w":"Power [kW]",
+    "cons_number":"#Product"
     }
+
 
 
 #pio.templates.default = "seaborn"
 
 pio.templates["custom_theme"] = go.layout.Template(
     layout=go.Layout(
-        #paper_bgcolor='rgba(0,0,0,0)', 
-        plot_bgcolor="rgba(230,230,230,255)",
-
+        paper_bgcolor='rgba(14,17,23,255)', 
+        plot_bgcolor='rgba(14,17,23,255)',
+        font_color="white",
+        font_size = 18,
         colorway=px.colors.qualitative.D3
 
     )
@@ -58,13 +83,14 @@ pio.templates["custom_theme"] = go.layout.Template(
 
 pio.templates.default = 'custom_theme'
 
+chart_width = 600
+
+
 ################## CONTENT START
 
 ############ TITLE
 
 st.title("📊 Exploratory Data Analysis")
-
-
 
 ###### SUBHEADER
 
@@ -72,79 +98,74 @@ st.write("")
 st.subheader("Training Dataset")
 st.markdown("""
     - **split:** training dataset represents  70% of all observations
-    - **shape:** 14 features x 7.000 observations
-    - **categorical values:** xyz
-    - **numerical values:** abc
-    
+    - **shape:** 8 features x 7.000 observations
+    - **categorical values:** Type and all failure categories
+    - **numerical values:**  Air temperature [K], Process temperature [K], Rotational speed [RPM], Torque [Nm], Tool wear [min]    
     """)
-df_train
+st.dataframe(df_train.rename(columns=lab_dict), width=chart_width)
+
 ###### SUBHEADER
 
 st.write("")
 st.subheader("Distribution of Numerical Data")
-st.markdown("Text...")
-st.markdown("Observation outside inter quartile range (IQR) will not be removed as the are part of the valid data distribution.")
+st.markdown("Observations outside inter quartile range (IQR) will not be removed as they are part of the valid data distribution.")
 
-#col_sort = "udi"
-col_grouping = ["machine_failure"]
-plot_list = [
-    "air_temperature_k", 
-    "process_temperature_k", 
-    "rotational_speed_rpm", 
-    "torque_nm", 
-    "tool_wear_min",
-    "machine_failure"
-]
-size = 600
+
+# generate plots and text 
+col_grouping = "machine_failure"
+
+# 1. feature to plot 2. comment on plot
+plot_list = {
+    #"air_temperature_k", 
+    1:["process_temperature_k", "Higher process temperature increases probability of machine failure."],
+    2:["rotational_speed_rpm", "Low rotational speed increases probability of machine failure."],
+    3:["torque_nm", "Higher torque increases probability of machine failure."],
+    4:["power_w", "Higher machine power (torque / rotation speed ) increases probability of machine failure."],
+    5:["machine_failure", "Dataset is imbalanced (~30:1)"]    
+}
+
+
 df_chart = df_train#.sort_values(col_sort)
 
-for column in plot_list:
-    for group in col_grouping:
-        fig = px.histogram(
-            data_frame = df_chart,
-            x = column,
-            color = group,
-            #width = size,
-            #height = size,
-            opacity=0.7,
-            barmode="overlay",
-            labels=lab_dict,
-            title=f"Histogram of {lab_dict[column]}",
-            marginal="box"
-            )
-        fig.update_layout(legend=dict(
-        orientation="h",
-        yanchor="top",
-        y=-0.20,
-        ))
-        #fig.show()
-        st.plotly_chart(fig, width = size)
+for key, value in plot_list.items():
+    st.plotly_chart(f.chart_hist_box(df_chart, value[0], col_grouping, lab_dict), width=chart_width)
+    
+    st.markdown(f"**Note:** {value[1]}")
 
+# reveal slides
+markdown_content = f"""
 
-###### SUBHEADER
+## {lab_dict[plot_list[1][0]]}
 
-st.write("")
-st.subheader("Box Plot of Selected Data")
-st.markdown("Observations outside inter quartile range (IQR) will not be removed as the are part of the valid data distribution.")
+{f.embed_plotly_in_iframe(f.chart_hist_box(df_chart, plot_list[1][0], col_grouping, lab_dict))}
+{plot_list[1][1]}
+---
+## {lab_dict[plot_list[2][0]]}
 
-#size = 400
+{f.embed_plotly_in_iframe(f.chart_hist_box(df_chart, plot_list[2][0], col_grouping, lab_dict))}
+{plot_list[2][1]}
+---
+## {lab_dict[plot_list[3][0]]}
 
-cols = [ "air_temperature_k",
-"process_temperature_k",
- "rotational_speed_rpm",
- "torque_nm",
- "tool_wear_min",]
-df_chart = df_train
-for col in cols:
-    fig = px.box(
-        df_chart, 
-        y=col, 
-        #width=size, 
-        #height=size, 
-        labels=lab_dict,
-        )
-    #fig.show()
-    st.plotly_chart(fig, width= size)
+{f.embed_plotly_in_iframe(f.chart_hist_box(df_chart, plot_list[3][0], col_grouping, lab_dict))}
+{plot_list[3][1]}
+---
+## {lab_dict[plot_list[4][0]]}
+
+{f.embed_plotly_in_iframe(f.chart_hist_box(df_chart, plot_list[4][0], col_grouping, lab_dict))}
+{plot_list[4][1]}
+---
+## {lab_dict[plot_list[5][0]]}
+
+{f.embed_plotly_in_iframe(f.chart_hist_box(df_chart, plot_list[5][0], col_grouping, lab_dict))}
+{plot_list[5][1]}
+
+"""
+# Präsentation in Streamlit anzeigen
+rs.slides(
+    markdown_content, 
+    theme="black" 
+)
 
 
 ###### SUBHEADER
@@ -153,65 +174,33 @@ st.write("")
 st.subheader("Scatter Plot of Selected Data")
 
 
-# individual scatter plots
-#size = 400
-scatter_color ="machine_failure"
+# generate plots and text 
 df_chart = df_train#[df_train["machine_failure"]==1]
 
-scatter_pairs = {
-    1:[ 'rotational_speed_rpm','torque_nm',  scatter_color],
-    #2:[ 'torque_nm', 'tool_wear_min', scatter_color],
-    3:[ 'torque_nm', 'tool_wear_min',  scatter_color],
-    #4:['cons_number', 'udi',  scatter_color],
-    #5:[ 'udi','torque_nm',  scatter_color],
-    #6:[ 'cons_number','torque_nm',  scatter_color]
-    7:["air_temperature_k", "process_temperature_k",scatter_color ]
+# list in dict: 1. x value 2. y value 3. grouping (color) 4. comment
+scatter_pairs = {    
+    1:[ 'rotational_speed_rpm','torque_nm',  "pwf", "Higher machine power (torque / rotation speed) increases probability of failure."],
+    2:["tool_wear_min", 'torque_nm', "osf", "High torque combined with long tool wear time increases probability of failure." ]
     }
 
+for key, value in scatter_pairs.items():
+    st.plotly_chart(f.chart_scatter(df_chart, value[0], value[1],value[2], lab_dict), width= chart_width)
+    
+    st.markdown(f"**Note:** {value[3]}")
+    st.markdown("")
 
-for key, values in scatter_pairs.items():
-    fig = px.scatter(
-        df_chart,
-        x = values[0],
-        y = values[1],
-        color = values[2],
-        opacity = 0.7,
-        #width=size,
-        #height=size,
-        labels=lab_dict,
-        title=f"{lab_dict[values[0]]} vs {lab_dict[values[1]]}"
-
-        #        color_continuous_scale = ["green", "red"]
-    )
-    fig.update_layout(legend=dict(
-        orientation="h",
-        yanchor="top",
-        y=-0.30,
-   ))
-
-    #fig.show()
-    st.plotly_chart(fig, width= size)
 
 ###### SUBHEADER
 
-#size=1000
-
 st.write("")
 st.subheader("Correlation matrix")
-st.markdown("- **Highly negatively correlated columns:** \n"
-            "- **Highly positively correlated columns:** \n"
-            
+st.markdown("- **Highly negatively correlated columns:** \n" \
+            "   - torque vs rotational speed\n"
+            "- **Highly positively correlated columns:** \n" \
+            "   - air temperature vs process temperature"
             )
+features_included = ["air_temperature_k","process_temperature_k","rotational_speed_rpm","torque_nm","tool_wear_min"]
 
-df_train_corr = df_train.select_dtypes(include=np.number).corr()
+df_chart = df_train#[df_train["machine_failure"]==1]
+st.plotly_chart(f.chart_corr(df_chart, features_included,lab_dict),width=chart_width)
 
-
-fig = px.imshow(
-    df_train_corr, 
-    text_auto=True, 
-    color_continuous_scale=[px.colors.qualitative.D3[0],"white",px.colors.qualitative.D3[1]], 
-    zmin=-1,  
-    zmax=1,
-    labels=lab_dict)
-
-st.plotly_chart(fig,width=size)
--- st_function.py
+++ st_function.py
@@ -2,9 +2,12 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 import plotly.io as pio
+import plotly.express as px
 #import duckdb
 from sklearn.model_selection import train_test_split
 #import streamlit as st
+from typing import Union
+import base64 # Neu: Für die Kodierung des HTML-Inhalts
 
 def import_stuff():
     import plotly.express as px
@@ -21,3 +24,70 @@ def navigation():
     st.sidebar.page_link("pages/01_eda.py", label="📊 EDA")
     #st.sidebar.page_link("pages/02_pipeline.py", label="📜 Pipeline")
 
+def chart_hist_box(data_frame, column, group, lab_dict):
+    fig = px.histogram(
+        data_frame = data_frame,
+        x = column,
+        color = group,
+        opacity=0.7,
+        barmode="overlay",
+        labels=lab_dict,
+        #title=f"Histogram of {lab_dict[column]}",
+        marginal="box",
+        
+        )
+    fig.update_layout(legend=dict(
+    orientation="h",
+    yanchor="top",
+    y=-0.20,
+    ))
+    return fig
+
+def chart_scatter(data_frame, x, y, color, lab_dict):
+    fig = px.scatter(
+        data_frame,
+        x = x,
+        y = y,
+        color = color,
+        opacity = 0.7,
+        labels=lab_dict,
+        title=f"{lab_dict[x]} vs {lab_dict[y]}"
+    )
+    fig.update_layout(legend=dict(
+        orientation="h",
+        yanchor="top",
+        y=-0.30,
+    ))
+    return fig
+
+def chart_corr(data_frame,features_included,lab_dict ):
+    corr=data_frame[features_included].corr()
+    fig = px.imshow(
+        corr, 
+        text_auto=".1f", 
+        color_continuous_scale=[px.colors.qualitative.D3[0],"white",px.colors.qualitative.D3[1]], 
+        zmin=-1,  
+        zmax=1,
+        x=[lab_dict[x] for x in features_included],
+        y=[lab_dict[x] for x in features_included]
+
+    )
+    return fig
+
+def embed_plotly_in_iframe(fig: Union[go.Figure, dict], height: int = 550) -> str:
+   
+    chart_html_full = fig.to_html(
+        include_plotlyjs='cdn', 
+        full_html=True,
+        div_id='plotly_reveal_embed'
+    )
+    
+   
+    html_bytes = chart_html_full.encode('utf-8')
+    encoded = base64.b64encode(html_bytes).decode('utf-8')
+    data_uri = f"data:text/html;base64,{encoded}"
+    
+   
+    iframe_tag = f'<iframe src="{data_uri}" style="width:100%; height:{height}px; border:none; background-color: grey;"></iframe>'
+    
+    return iframe_tag
